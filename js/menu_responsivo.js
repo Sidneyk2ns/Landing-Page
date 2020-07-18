303629be0a773bab21f63c72ffe8b8505ef45187
Sidney Alves <sidney.kns@gmail.com>
2020-07-18 20:06:47 -0300

    menu dinamico
    menu com scroll dinâmico

--- js/menu_responsivo.js
+++ js/menu_responsivo.js
@@ -4,4 +4,30 @@ $(function() {
         $('.menu-mobile').find('ul').slideToggle();
     });
 
+
+    $('nav li').click(function(){
+		var href = $(this).attr('li');
+		var offSetTop = $(href).offset().top;
+		$('html, body').animate({'scrollTop':offSetTop});
+
+		return false;
+	});
+
+	$('nav li a').click(function(){
+    	if($('nav li a').css('color','#13e8ef'))
+           $(this).css('color','#489596') 
+		
+	});
+
+	$('nav.menu-mobile i').click(function() {
+                var el = $(this).parent().find('ul')
+                if (el.is(':visible') == false) {
+                    el.fadeIn();
+                } else {
+                    el.fadeOut();
+                }
+
+            })
+
 });
+
